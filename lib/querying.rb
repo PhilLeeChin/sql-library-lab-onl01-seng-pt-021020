0ff8adde886b57882c1046e3095023975b37ae25
Philip Lee-Chin <phil.leechin@outlook.com>
2020-04-19 20:53:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,9 +11,9 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT (*) FROM characters GROUP BY species ORDER BY COUNT (species) DESC LIMIT 1;"
 end
 
-# def select_name_and_series_subgenres_of_authors
-#   "SELECT authors.name, subgenres.name FROM authors, JOIN series ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id;"
-# end
+def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name FROM authors, JOIN series ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id;"
+end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series JOIN books ON books.series_id = series_id JOIN character_books ON character_books.book_id = book_id JOIN characters ON character_books.character_id = character_id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1;"
